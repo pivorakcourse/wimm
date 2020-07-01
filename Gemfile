120a84aad81af63123963ddaa9a7303c616331d9
Volodya Sveredyuk <sveredyuk@gmail.com>
2020-07-01 15:18:15 +0300

    Added devise

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.5'
 
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'devise', '~> 4.7', '>= 4.7.2'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root 'home#index'
+
+  scope 'app' do
+    get '/', to: 'dashboard#index'
+  end
 end
