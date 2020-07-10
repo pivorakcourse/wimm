27e9d2683da2913475428868c35eb91f561589ae
Sarutoh <sarutoh@outlook.com>
2020-07-10 12:37:31 +0300

    fixed helper with rubicop

--- app/helpers/destroy_account_helper.rb
+++ app/helpers/destroy_account_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module DestroyAccountHelper
   def delete_account_button(account)
-    if RemoveAccountPolicy.new(account).allowed?
-      link_to "Delete", account_path(account), action: "destroy",
-      id: account.id, method: :delete, class: 'button button secondary', data: { confirm: 'Are you sure?'}
-    end
+    return unless RemoveAccountPolicy.new(account).allowed?
+
+    link_to 'Delete', account_path(account), action: 'destroy',
+    id: account.id, method: :delete, class: 'button button secondary', data: { confirm: 'Are you sure?' }
   end
 end
