4c187ea5bfab25a7aa7ca5c13a940da82957b22d
irina <irina.zorij@gmail.com>
2020-07-17 10:32:13 +0000

    As a user after registration I will have pre seed data set

---
+++ spec/services/pre_seed_user_service_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PreSeedUserService do
+  let(:user) { create(:user) }
+
+  subject { described_class.new(user) }
+
+  it 'successfully create account' do
+    expect do
+      subject.call
+    end.to change { Account.count }.from(0).to(2)
+
+    expect(user.accounts.count).to eq 2
+    expect(user.accounts).to include(
+      have_attributes(name: 'Cash'),
+      have_attributes(name: 'Credit Card')
+    )
+  end
+
+  it 'successfully create category' do
+    expect do
+      subject.call
+    end.to change { Category.count }.from(0).to(8)
+
+    expect(user.categories.count).to eq 8
+    expect(user.categories).to include(
+      have_attributes(name: 'Home'),
+      have_attributes(name: 'Food'),
+      have_attributes(name: 'Sport'),
+      have_attributes(name: 'Learning'),
+      have_attributes(name: 'Cloth'),
+      have_attributes(name: 'Car'),
+      have_attributes(name: 'Travel'),
+      have_attributes(name: 'Fun')
+    )
+  end
+end
